2d76a13824db437f4195ea629c93b262144babd6
Andrew Pritykin <apritykin@gmail.com>
2015-12-22 19:25:15 -0500

    First commit

---
+++ app/src/main/java/com/leetinsider/edittextexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.leetinsider.edittextexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.edittext);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = editText.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
